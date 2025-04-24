d8d6b05ce415182caba191ab3928a5c13d569563
Varun-111 <110555164+Varun-111@users.noreply.github.com>
2025-04-24 10:54:10 +0530

    Update OlaScriptSQL.sql

--- OlaScriptSQL.sql
+++ OlaScriptSQL.sql
@@ -5,4 +5,4 @@ Create View Successful_Bookings as
 Select * from OlaBookings Where Booking_Status = 'Success';
 Select * from Successful_Bookings
 
-#2. Find the average ride distance for each vehcile type.
+
